c56a96691bed9afa84095a5ad7d9501b3e551649
the1NotHuman <55295668+the1NotHuman@users.noreply.github.com>
2022-02-07 18:20:52 -0600

    .

--- src/main/java/frc/robot/controllers/WolfbyteJoystick.java
+++ src/main/java/frc/robot/controllers/WolfbyteJoystick.java
@@ -6,7 +6,6 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.Subsystem;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants;
 import frc.robot.commands.DriveWithJoystick;
 import frc.robot.commands.SignedCommand;
