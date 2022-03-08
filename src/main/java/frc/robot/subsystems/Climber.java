69e47ec3ecfff84f622dbbc85b9739b131ef0321
Serena <ignatiusrobotics@gmail.com>
2022-03-08 17:22:36 -0600

    Updated to TalonFX

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.Servo;
@@ -8,7 +9,7 @@ import frc.robot.Constants;
 
 public class Climber extends SimpleMotorSubsystem{
     
-    private final WPI_TalonSRX CLimberMotor = new WPI_TalonSRX(Constants.Climber.CLIMBER_MOTOR_ID);
+    private final WPI_TalonFX CLimberMotor = new WPI_TalonFX(Constants.Climber.CLIMBER_MOTOR_ID);
     private final Servo servo = new Servo(Constants.Climber.CLIMBER_SERVO_ID);
     private boolean locked;
 
