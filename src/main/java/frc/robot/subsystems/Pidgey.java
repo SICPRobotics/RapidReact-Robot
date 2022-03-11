54c7f1817b1e9d7f8de1f0e248a6867f8e8a8ef2
Serena <ignatiusrobotics@gmail.com>
2022-03-11 17:40:26 -0600

    Update Pidgey.java

--- src/main/java/frc/robot/subsystems/Pidgey.java
+++ src/main/java/frc/robot/subsystems/Pidgey.java
@@ -1,18 +1,64 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.sensors.Pigeon2;
+import com.ctre.phoenix.sensors.Pigeon2.AxisDirection;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.SubsystemBaseWrapper;
 
 public class Pidgey extends SubsystemBaseWrapper {
     public final Pigeon2 pigeon = new Pigeon2(10);
+    private final double[] vXYZ = new double[3];
 
-    public double getRotation() {
+    public Pidgey() {
+        super();
+
+        pigeon.configMountPose(AxisDirection.PositiveY, AxisDirection.NegativeX);
+        pigeon.setYaw(0);
+        pigeon.configMountPosePitch(6.10);
+    }
+    
+    /**
+     * 60 degrees at arm up, 0 deg at arm down. No calibration needed
+     * @return
+     */
+    public double getArmRotation() {
+        return -pigeon.getPitch() + 6.10;
+    }
+
+    public Pidgey resetRobotHeading() {
+        pigeon.setYaw(0);
+        return this;
+    }
+
+    /**
+     * Gets the robot heading relative to start position.
+     * Automatically calibrated at startup; use resetRobotHeading to calibrate again.
+     * @return
+     */
+    public double getRobotRelativeHeading() {
         return pigeon.getYaw();
     }
 
+    /**
+     * Get robot compass heading (relative to north pole). No calibration needed
+     * @return
+     */
+    public double getRobotAbsoluteHeading() {
+        return pigeon.getAbsoluteCompassHeading();
+    }
+
+    /* Not tested yet
+    public double getAngularVelocity() {
+        pigeon.getRawGyro(vXYZ);
+        return vXYZ[2];
+    }*/
+
     @Override
     public void periodic() {
-        System.out.println(getRotation());
+        SmartDashboard.putNumber("Pigeon Yaw", pigeon.getYaw());
+        SmartDashboard.putNumber("Pigeon Pitch", pigeon.getPitch());
+        SmartDashboard.putNumber("Pigeon Roll", pigeon.getRoll());
+        SmartDashboard.putNumber("Pigeon Arm Rotation", getArmRotation());
     }
 }
