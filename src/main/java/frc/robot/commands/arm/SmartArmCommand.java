1c01b05aaa379d9ae8ad84fce4fc2cb921b03596
Will <ignatiusrobotics@gmail.com>
2022-03-15 13:02:48 -0500

    changed errror threshhods for  SmartArmCommands

--- src/main/java/frc/robot/commands/arm/SmartArmCommand.java
+++ src/main/java/frc/robot/commands/arm/SmartArmCommand.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.arm;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.CargoArm;
 import frc.robot.subsystems.Pidgey;
@@ -8,17 +9,20 @@ public abstract class SmartArmCommand extends CommandBase {
     public final Pidgey pidgey;
     public final CargoArm arm;
     public final double target;
-    public double progress = 0;
-    public SmartArmCommand(CargoArm arm, Pidgey pidgey, double target) {
+    public double error = 0;
+    public final double maxError;
+    public SmartArmCommand(CargoArm arm, Pidgey pidgey, double target, double maxError) {
         this.arm = arm;
         this.pidgey = pidgey;
         this.target = target;
+        this.maxError = maxError;
         addRequirements(arm);
     }
 
     @Override
     public void execute() {
-        progress = target - pidgey.getArmRotation();
+        error = (target - pidgey.getArmRotation()) / maxError;
+        SmartDashboard.putNumber("Arm Progress", error);
         arm.setMotor(getOutput());
     }
 
