5908558ade69a6710689346f00a449a4422c5211
the1NotHuman <55295668+the1NotHuman@users.noreply.github.com>
2022-05-20 21:37:25 -0500

    first try at Driver motor PI control

--- src/main/java/frc/robot/subsystems/SwerveWheel.java
+++ src/main/java/frc/robot/subsystems/SwerveWheel.java
@@ -11,12 +11,19 @@ public class SwerveWheel {
 
     private TalonFX driveMotor;
     private TalonFX steerMotor;
-    private double setpoint = 0;
+    private double steerSetpoint = 0;
+    private double driveSetpoint = 0;
     private double steerVelocity = 0;
-    private final double maxError = Math.PI * 2;
-    private double P = 1;
-    private double I = 0.25;
+    private double steerTime = 0;
+    private double driveTime = 0;
+    private final double MAX_STEER_ERROR = Math.PI * 2;
+    private final double MAX_DRIVE_ERROR = 10 * 12; // 10ft in inches
+    private double sP = 1;
+    private double sI = 0.25;
+    private double dP = 1;
+    private double dI = 0.5;
     private Timer timer = new Timer();
+    private final double WHEEL_Diameter_INCHES = 6;
 
     public SwerveWheel(int driveMotorId, int steerMotorId){
         this.driveMotor = new TalonFX(driveMotorId);
@@ -24,6 +31,7 @@ public class SwerveWheel {
         motorInit(this.driveMotor);
         motorInit(this.steerMotor);
         this.timer.reset();
+        this.timer.start();
     }
 
     private void motorInit(TalonFX motor){
@@ -35,29 +43,42 @@ public class SwerveWheel {
     public void setDriveMotor(double velocity){
         this.driveMotor.set(TalonFXControlMode.PercentOutput, velocity);
     }
-    public void setSteerMotor(double setpoint){
-        this.setpoint = setpoint;
-        this.timer.start();
+    public void setSteerMotor(double steerSetpoint){
+        this.steerSetpoint = steerSetpoint;
+        this.steerTime = this.timer.get();
+    }
+    public void setDriveSetpoint(double driveSetpoint){
+        this.driveSetpoint = driveSetpoint;
+        this.driveTime = this.timer.get();
     }
     public double getDriveEncoder(){
-        return this.driveMotor.getSelectedSensorPosition();
+        return this.driveMotor.getSelectedSensorPosition() * 4096 * WHEEL_Diameter_INCHES;
     }
     public double getSteerEncoder(){
         return this.steerMotor.getSelectedSensorPosition() * (4096/2) * Math.PI;
     }
-    public double getSetpoint() {
-        return setpoint;
+    public double getsteerSetpoint() {
+        return steerSetpoint;
     }
+    public double getDriveSetpoint() {
+        return driveSetpoint;
+    }
+    public void resetEncoders(){
+        this.driveMotor.setSelectedSensorPosition(0);
+        this.steerMotor.setSelectedSensorPosition(0);
+    }
+
 
     public void updateSteer(){
-        double error = (this.setpoint - getSteerEncoder()) / this.maxError; 
-        if(error == 0){
-            this.timer.reset();
-        }
-        double iError = this.timer.get() * error;
-        this.steerMotor.set(TalonFXControlMode.PercentOutput, (error * P) + (iError * I)); // PI Controller 
+        double error = (this.steerSetpoint - getSteerEncoder()) / this.MAX_STEER_ERROR; 
+        double iError = (this.timer.get() - this.steerTime) * error;
+        this.steerMotor.set(TalonFXControlMode.PercentOutput, (error * sP) + (iError * sI)); // PI Controller 
     }
-
+    public void updateDrive(){
+        double error = (this.driveSetpoint - getDriveEncoder()) / this.MAX_DRIVE_ERROR; 
+        double iError = (this.timer.get() - this.driveTime) * error;
+        this.steerMotor.set(TalonFXControlMode.PercentOutput, (error * dP) + (iError * dI)); // PI Controller
+    }
 
 
 }
